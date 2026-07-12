2e90ee555a40f10c39e630b8f552db44c1b13f4b
tyriquezim <zimbizi.tyrique@gmail.com>
2026-07-12 10:41:28 +0800

    Added React component MealCard for holding meal info

---
+++ src/components/MealCard.jsx
@@ -0,0 +1,12 @@
+function MealCard({meal})
+{
+    return (
+        <div className="meal-card">
+            <img src={meal.strMealThumb} alt={meal.strMeal} />
+            <div className="meal-card-info">
+                <h3>{meal.strMeal}</h3>
+                <p>{meal.strCategory}</p>
+            </div>
+        </div>
+    );
+}
