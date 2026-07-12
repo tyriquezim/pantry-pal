cebf1b245b5190e06b96fe5d59e00dc486528e4f
tyriquezim <zimbizi.tyrique@gmail.com>
2026-07-12 17:13:53 +0800

    Added MealDB query functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home"
+import ProtectedRoute from "./components/ProtectedRoute";
+import NavBar from "./components/NavBar";
+import { useAuth } from "./context/AuthContext";
 
 function App() 
 {
-  return AppRoutes();
+  const {user} = useAuth();
+  return <AppRoutes user={user}/>
 }
 
-function AppRoutes()
+function AppRoutes(props)
 {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/login" element={props.user ? <Navigate to="/"/> : <Login />} />
+        <Route path="/register" element={props.user ? <Navigate to="/"/> : <Register />} />
+        <Route path="/" element={
+          <ProtectedRoute> 
+            <Home /> 
+          </ProtectedRoute>} />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
